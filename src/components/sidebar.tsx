9ab51fa8bd5f6600d2c86906a7ba9b737a5c85bf
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-05-08 22:51:13 +0500

    feat: finish sidebar ui setup

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,29 +1,48 @@
 import {
-  LayoutGrid,
+  Home,
   Library,
   ListMusic,
   Mic2,
   Music,
   Music2,
-  PlayCircle,
-  Radio,
+  Search,
   User,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea } from "./ui/scroll-area";
 
 interface Props {
   playlists: any[];
 }
 
+const sidebarLinks = [
+  {
+    title: "Discover",
+    links: [
+      { title: "Home", icon: <Home className="mr-2 h-4 w-4" /> },
+      { title: "Search", icon: <Search className="mr-2 h-4 w-4" /> },
+    ],
+  },
+  {
+    title: "Library",
+    links: [
+      { title: "Playlists", icon: <ListMusic className="mr-2 h-4 w-4" /> },
+      { title: "Songs", icon: <Music2 className="mr-2 h-4 w-4" /> },
+      { title: "Made for You", icon: <User className="mr-2 h-4 w-4" /> },
+      { title: "Artists", icon: <Mic2 className="mr-2 h-4 w-4" /> },
+      { title: "Albums", icon: <Library className="mr-2 h-4 w-4" /> },
+    ],
+  },
+];
+
 export function Sidebar({ playlists }: Props) {
   return (
     <div className="border-r">
-      <div className="space-y-4 py-4">
-        <div className="px-4 py-2">
-          <h1 className="flex items-center gap-2 px-2 mb-4 font-bold text-2xl">
-            awen <Music />
-          </h1>
+      <h1 className="flex items-center gap-2 px-6 mt-4 font-bold text-2xl">
+        awen <Music />
+      </h1>
+      <div className="space-y-4">
+        {/* <div className="px-4 py-2">
           <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
             Discover
           </h2>
@@ -33,66 +52,61 @@ export function Sidebar({ playlists }: Props) {
               size="sm"
               className="w-full justify-start"
             >
-              <PlayCircle className="mr-2 h-4 w-4" />
-              Listen Now
+              <Home className="mr-2 h-4 w-4" />
+              Home
             </Button>
             <Button variant="ghost" size="sm" className="w-full justify-start">
-              <LayoutGrid className="mr-2 h-4 w-4" />
-              Browse
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <Radio className="mr-2 h-4 w-4" />
-              Radio
+              <Search className="mr-2 h-4 w-4" />
+              Search
             </Button>
           </div>
-        </div>
-        <div className="px-4 py-2">
-          <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
-            Library
-          </h2>
-          <div className="space-y-1">
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <ListMusic className="mr-2 h-4 w-4" />
-              Playlists
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <Music2 className="mr-2 h-4 w-4" />
-              Songs
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <User className="mr-2 h-4 w-4" />
-              Made for You
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <Mic2 className="mr-2 h-4 w-4" />
-              Artists
-            </Button>
-            <Button variant="ghost" size="sm" className="w-full justify-start">
-              <Library className="mr-2 h-4 w-4" />
-              Albums
-            </Button>
+          <hr className="mt-4" />
+        </div> */}
+        <div>
+          <div className="px-4 py-2">
+            {sidebarLinks.map((section) => (
+              <div key={section.title} className="py-4">
+                <h2 className="mb-2 px-2 text-lg font-semibold tracking-tight">
+                  {section.title}
+                </h2>
+                <div className="space-y-1">
+                  {section.links.map((link) => (
+                    <Button
+                      key={link.title}
+                      variant="ghost"
+                      size="sm"
+                      className="w-full justify-start"
+                    >
+                      {link.icon}
+                      {link.title}
+                    </Button>
+                  ))}
+                </div>
+                <hr className="mt-4" />
+              </div>
+            ))}
           </div>
         </div>
-        <div className="py-2">
-          <h2 className="relative px-6 text-lg font-semibold tracking-tight">
-            Playlists
-          </h2>
-          <ScrollArea className="flex-1 px-2">
-            <div className="space-y-1 p-2">
-              {playlists?.map((playlist) => (
-                <Button
-                  key={playlist}
-                  variant="ghost"
-                  size="sm"
-                  className="w-full justify-start font-normal"
-                >
-                  <ListMusic className="mr-2 h-4 w-4" />
-                  {playlist}
-                </Button>
-              ))}
-            </div>
-          </ScrollArea>
-        </div>
+      </div>
+      <div className="py-2">
+        <h2 className="relative px-6 text-lg font-semibold tracking-tight">
+          Playlists
+        </h2>
+        <ScrollArea className="flex-1 px-2">
+          <div className="space-y-1 p-2">
+            {playlists?.map((playlist) => (
+              <Button
+                key={playlist}
+                variant="ghost"
+                size="sm"
+                className="w-full justify-start font-normal"
+              >
+                <ListMusic className="mr-2 h-4 w-4" />
+                {playlist}
+              </Button>
+            ))}
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
